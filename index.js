890cade06188531ff29a53a5eb25c7364492fee4
Nhlanhla Simelane <nhlanhla@Nhlanhlas-MacBook-Pro.local>
2022-12-16 08:40:18 +0200

    uploading server

---
+++ index.js
@@ -0,0 +1,36 @@
+//IMPORT FROM PACKAGES
+const express = require("express");
+const mongoose = require("mongoose");
+const adminRouter = require("./routes/admin");
+
+//IMPORT FROM OTHER FILES
+const authRouter = require("./routes/auth");
+const productRouter = require("./routes/product");
+const userRouter = require("./routes/user");
+
+//INITIALISATIONS
+const PORT = 3000;
+const app = express();
+const DB =
+  "mongodb+srv://savvyly:purpose1@cluster0.nemegie.mongodb.net/?retryWrites=true&w=majority";
+
+//middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRouter);
+app.use(userRouter);
+
+//Connections
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
